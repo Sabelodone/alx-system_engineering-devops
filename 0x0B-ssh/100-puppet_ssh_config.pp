5b202011c11d9e63ef0583ee5a20391bec75a6da
Sabelodone <sabelozondo825gmail.com>
2023-11-27 23:25:27 -0800

    private key

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+content => "
+    # SSH client configuration
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
+}
