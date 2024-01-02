78f81a68380c32c08f87f3c41a653eb485e1cdb4
Sabelodone <sabelozondo825gmail.com>
2024-01-02 05:58:19 -0800

    Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,36 +1,30 @@
 #!/usr/bin/python3
+"""For a given employee ID, returns information about
+their TODO list progress"""
+
 import requests
 import sys
 
-def fetch_employee_todo_progress(employee_id):
-    base_url = 'https://jsonplaceholder.typicode.com'
-    employee_url = f'{base_url}/users/{employee_id}'
-    todos_url = f'{base_url}/todos?userId={employee_id}'
-
-    try:
-        employee_response = requests.get(employee_url)
-        todos_response = requests.get(todos_url)
-
-        employee_data = employee_response.json()
-        todos_data = todos_response.json()
-
-        if 'name' in employee_data and 'id' in employee_data:
-            employee_name = employee_data['name']
-            total_tasks = len(todos_data)
-            completed_tasks = [task['title'] for task in todos_data if task['completed']]
-
-            print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
-            for task_title in completed_tasks:
-                print(f"\t{task_title}")
-
-    except requests.RequestException as e:
-        print(f"Error fetching data: {e}")
-        sys.exit(1)
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
 
-    employee_id = int(sys.argv[1])
-    fetch_employee_todo_progress(employee_id)
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+
+    name = user.json().get('name')
+
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    totalTasks = 0
+    completed = 0
+
+    for task in todos.json():
+        if task.get('userId') == int(userId):
+            totalTasks += 1
+            if task.get('completed'):
+                completed += 1
+
+    print('Employee {} is done with tasks({}/{}):'
+          .format(name, completed, totalTasks))
+
+    print('\n'.join(["\t " + task.get('title') for task in todos.json()
+          if task.get('userId') == int(userId) and task.get('completed')]))
